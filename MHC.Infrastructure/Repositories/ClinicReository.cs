ea37ff41f98f881994cb3e2ebf186be378146a40
Moncef <moncefelhilali@gmail.com>
2024-01-14 22:27:03 +0100

    Clinic Repository

---
+++ MHC.Infrastructure/Repositories/ClinicReository.cs
@@ -0,0 +1,13 @@
+﻿using MHC.Domain.Entities;
+using MHC.Domain.Interfaces;
+using MHC.Infrastructure.Data;
+
+namespace MHC.Infrastructure.Repositories
+{
+    public class ClinicReository : GenericRepository<Clinic>, IClinicRepository
+    {
+        public ClinicReository(DBC dbc) : base(dbc) { }
+        
+        // Additional Methods
+    }
+}
