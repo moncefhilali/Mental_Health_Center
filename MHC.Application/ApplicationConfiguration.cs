cd910b51a878537af61ccf108a3f581e5c41e942
Moncef <moncefelhilali@gmail.com>
2024-01-15 15:22:26 +0100

    Add MediatR & Repositories Services

--- MHC.Application/ApplicationConfiguration.cs
+++ MHC.Application/ApplicationConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using MHC.Application.Mapper;
+﻿using MediatR;
+using MHC.Application.Clinics.Queries;
+using MHC.Application.Mapper;
+using MHC.Application.Services.Queries;
+using MHC.Application.Treatments.Queries;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MHC.Application
@@ -8,6 +12,9 @@ namespace MHC.Application
         public static IServiceCollection AddDependecyInjectionApplication(this IServiceCollection services)
         {
             // MediatR
+            services.AddMediatR(typeof(GetAllClinicsQuery));
+            services.AddMediatR(typeof(GetAllServicesQuery));
+            services.AddMediatR(typeof(GetAllTreatmentsQuery));
 
             // Mapper
             services.AddAutoMapper(typeof(MappingProfile));
