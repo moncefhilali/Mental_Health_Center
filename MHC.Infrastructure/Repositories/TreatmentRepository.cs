0153bf7040320f298849bc8fa97ee9c45710e2ad
Moncef <moncefelhilali@gmail.com>
2024-01-15 10:00:10 +0100

    Treatment Repository

---
+++ MHC.Infrastructure/Repositories/TreatmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using MHC.Domain.Entities;
+using MHC.Domain.Interfaces;
+using MHC.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHC.Infrastructure.Repositories
+{
+    public class TreatmentRepository : GenericRepository<Treatment>, ITreatmentRepository
+    {
+        public TreatmentRepository(DBC dbc) : base(dbc) { }
+    }
+}
