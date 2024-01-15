8c122eb8bf467c45dfcefb1b8ccb0ea27879e814
Moncef <moncefelhilali@gmail.com>
2024-01-15 16:06:03 +0100

    Add Services Controller

---
+++ MHC.API/Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MHC.Application.Services.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ServicesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllServices()
+        {
+            var result = await _mediator.Send(new GetAllServicesQuery());
+            return Ok(result);
+        }
+    }
+}
