a5a76fa96a2a3e5ccad6cd41b30f63ad2de1c5f1
Moncef <moncefelhilali@gmail.com>
2024-01-15 14:56:28 +0100

    Clinics Query Handler

---
+++ MHC.Application/Clinics/Queries/GetAllClinicsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using MHC.Application.Clinics.ViewModel;
+using MHC.Domain.Interfaces;
+
+namespace MHC.Application.Clinics.Queries
+{
+    public class GetAllClinicsQueryHandler : IRequestHandler<GetAllClinicsQueries, List<ClinicViewModel>>
+    {
+        private readonly IClinicRepository _repository;
+        private readonly IMapper _mapper;
+        public GetAllClinicsQueryHandler(IClinicRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ClinicViewModel>> Handle(GetAllClinicsQueries request, CancellationToken cancellationToken)
+        {
+            var clinics = await _repository.GetAllAsync();
+            return _mapper.Map<List<ClinicViewModel>>(clinics);
+        }
+    }
+}
