46b53189b1a184eb6402538b5573a89b7c658d95
Moncef <moncefelhilali@gmail.com>
2024-01-15 11:57:43 +0100

    Treatments Query Handler

---
+++ MHC.Application/Treatments/Queries/GetAllTreatmentsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using MHC.Application.Treatments.ViewModel;
+using MHC.Domain.Interfaces;
+
+namespace MHC.Application.Treatments.Queries
+{
+    public class GetAllTreatmentsQueryHandler : IRequestHandler<GetAllTreatmentsQuery, List<TreatmentViewModel>>
+    {
+        private readonly ITreatmentRepository _repository;
+        private readonly IMapper _mapper;
+        public GetAllTreatmentsQueryHandler(ITreatmentRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TreatmentViewModel>> Handle(GetAllTreatmentsQuery request, CancellationToken cancellationToken)
+        {
+            var treatments = await _repository.GetAllAsync();
+            return _mapper.Map<List<TreatmentViewModel>>(treatments);
+
+        }
+    }
+}
