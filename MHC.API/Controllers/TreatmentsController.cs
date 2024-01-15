b8f62c02632c5955e0d0230f65892237cefb1ca3
Moncef <moncefelhilali@gmail.com>
2024-01-15 16:00:49 +0100

    Add Treatment Controller

---
+++ MHC.API/Controllers/TreatmentsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MHC.Application.Treatments.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreatmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TreatmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllTreatments()
+        {
+            var result = await _mediator.Send(new GetAllTreatmentsQuery());
+            return Ok(result);
+        }
+    }
+}
