946f7e376254bab7d5f5d3cd50f466f04db75e70
Moncef <moncefelhilali@gmail.com>
2024-01-15 15:13:52 +0100

    Rename Query

---
+++ MHC.API/Controllers/ClinicsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MHC.Application.Clinics.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ClinicsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
