d4f25f7971081175df123859b196461537a649fc
Moncef <moncefelhilali@gmail.com>
2024-01-16 14:45:36 +0100

    Add Categories CQRS

---
+++ MHC.Application/Categories/Queries/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using MHC.Application.Categories.ViewModel;
+using MHC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHC.Application.Categories.Queries
+{
+    public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, List<CategoryViewModel>>
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public GetAllCategoriesQueryHandler(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoryViewModel>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await _repository.GetAllAsync();
+            return _mapper.Map<List<CategoryViewModel>>(categories);
+        }
+    }
+}
---
+++ MHC.Application/Categories/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MHC.Domain.Entities;
+
+namespace MHC.Application.Categories.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public string? Name { get; set; }
+
+        public virtual ICollection<OurService> Services { get; set; } = new List<OurService>();
+    }
+}
