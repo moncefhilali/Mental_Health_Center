a03d27525a03412382734f55a410e8963a1b4f9d
Moncef <moncefelhilali@gmail.com>
2024-01-16 14:39:57 +0100

    Add ClinicTypes CQRS

---
+++ MHC.Application/ClinicTypes/Queries/GetAllClinicTypesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MHC.Application.ClinicTypes.ViewModel;
+
+namespace MHC.Application.ClinicTypes.Queries
+{
+    public class GetAllClinicTypesQuery : IRequest<List<ClinicTypeViewModel>>
+    {
+
+    }
+}
---
+++ MHC.Application/ClinicTypes/Queries/GetAllClinicTypesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using MHC.Application.ClinicTypes.ViewModel;
+using MHC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHC.Application.ClinicTypes.Queries
+{
+    public class GetAllClinicTypesQueryHandler : IRequestHandler<GetAllClinicTypesQuery, List<ClinicTypeViewModel>>
+    {
+        private readonly IClinicTypeRepository _repositoy;
+        private readonly IMapper _mapper;
+        public GetAllClinicTypesQueryHandler(IClinicTypeRepository repositoy, IMapper mapper)
+        {
+            _repositoy = repositoy;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ClinicTypeViewModel>> Handle(GetAllClinicTypesQuery request, CancellationToken cancellationToken)
+        {
+            var clinictypes = await _repositoy.GetAllAsync();
+            return _mapper.Map<List<ClinicTypeViewModel>>(clinictypes);
+        }
+    }
+}
