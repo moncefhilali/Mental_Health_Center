6fa59c799608651d51e3b0f003a33f5620129f12
Moncef <moncefelhilali@gmail.com>
2024-01-16 15:20:34 +0100

    Add ClinicTypes Controller

---
+++ MHC.API/Controllers/ClinicTypesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MHC.Application.ClinicTypes.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ClinicTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllClinicTypes()
+        {
+            var result = await _mediator.Send(new GetAllClinicTypesQuery());
+            return Ok(result);
+        }
+    }
+}
