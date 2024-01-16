27f1aa45dd0610062e6c5ddb33323c4cbeb7d53f
Moncef <moncefelhilali@gmail.com>
2024-01-16 15:17:12 +0100

    Add Images Controller

---
+++ MHC.API/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MHC.Application.Images.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ImagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllImages()
+        {
+            var result = await _mediator.Send(new GetAllImagesQuery());
+            return Ok(result);
+        }
+    }
+}
