4c333df3efbce98d7ec09a9e9468d0b87cdf9364
Moncef <moncefelhilali@gmail.com>
2024-01-16 15:12:33 +0100

    Add Doctors Controller

---
+++ MHC.API/Controllers/DoctorsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MHC.Application.Doctors.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DoctorsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllDoctors()
+        {
+            var result = await _mediator.Send(new GetAllDoctorsQuery());
+            return Ok(result);
+        }
+    }
+}
