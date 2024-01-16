b003666949a59935290bebef4486cbabb8b175d9
Moncef <moncefelhilali@gmail.com>
2024-01-16 14:09:37 +0100

    Add ClinicType Repository

---
+++ MHC.Domain/Interfaces/IClinicTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using MHC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHC.Domain.Interfaces
+{
+    public interface IClinicTypeRepository : IGenericRepository<ClinicType>
+    {
+        // Additional Methods
+    }
+}
---
+++ MHC.Infrastructure/Repositories/ClinicTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MHC.Domain.Entities;
+using MHC.Domain.Interfaces;
+using MHC.Infrastructure.Data;
+
+namespace MHC.Infrastructure.Repositories
+{
+    public class ClinicTypeRepository : GenericRepository<ClinicType>, IClinicTypeRepository
+    {
+        public ClinicTypeRepository(DBC context) : base(context) { }
+
+        // Additional Methods
+    }
+}
