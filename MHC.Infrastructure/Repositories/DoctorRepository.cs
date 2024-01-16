84dbce584b09990f99a7429bc5497481f98764d6
Moncef <moncefelhilali@gmail.com>
2024-01-16 14:05:32 +0100

    Add Doctor Repository

---
+++ MHC.Infrastructure/Repositories/DoctorRepository.cs
@@ -0,0 +1,16 @@
+﻿using MHC.Domain.Entities;
+using MHC.Domain.Interfaces;
+using MHC.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHC.Infrastructure.Repositories
+{
+    public class DoctorRepository : GenericRepository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(DBC context) : base(context) { }
+    }
+}
