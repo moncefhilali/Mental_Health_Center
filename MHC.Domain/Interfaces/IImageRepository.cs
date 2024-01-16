7fe2754a85ce3f72be1de5b7fbe168c4e345aecf
Moncef <moncefelhilali@gmail.com>
2024-01-16 14:15:05 +0100

    Add Image Repository

---
+++ MHC.Domain/Interfaces/IImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using MHC.Domain.Entities;
+
+namespace MHC.Domain.Interfaces
+{
+    public interface IImageRepository : IGenericRepository<Image>
+    {
+        // Additional Methods
+    }
+}
---
+++ MHC.Infrastructure/Repositories/ImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using MHC.Domain.Entities;
+using MHC.Domain.Interfaces;
+using MHC.Infrastructure.Data;
+
+namespace MHC.Infrastructure.Repositories
+{
+    public class ImageRepository : GenericRepository<Image>, IImageRepository
+    {
+        public ImageRepository(DBC context) : base(context) { }
+    }
+}
