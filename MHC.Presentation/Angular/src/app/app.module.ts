65c11c9c619496cc9f5ada54b30fcd1b92dde188
Moncef <moncefelhilali@gmail.com>
2024-01-17 23:04:26 +0100

    Introduction Component

--- MHC.Presentation/Angular/src/app/app.module.ts
+++ MHC.Presentation/Angular/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NavbarComponent, IntroductionComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
