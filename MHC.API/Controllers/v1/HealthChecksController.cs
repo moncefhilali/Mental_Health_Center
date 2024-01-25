32d9cee23110c400b3a9a03a0f95ccb2b95cc55c
Moncef <moncefelhilali@gmail.com>
2024-01-25 21:35:08 +0100

    Health Checks Controller

---
+++ MHC.API/Controllers/v1/HealthChecksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHC.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class HealthChecksController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public HealthChecksController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckHealth()
+        {
+            var result = await _httpClient.GetFromJsonAsync<object?>("https://localhost:7216/_health");
+            return Ok(result);
+        }
+    }
+}
